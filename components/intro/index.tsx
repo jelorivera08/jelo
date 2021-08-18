0f1f89fe2b978a01a2415e37963d5e551960a5f9
jelo <jelorivera08@gmail.com>
2021-08-18 13:29:50 +0800

    more animations

--- components/intro/index.tsx
+++ components/intro/index.tsx
@@ -6,19 +6,21 @@ import cs from "classnames";
 
 const Intro = () => (
   <div className={classes.container}>
-    <h1 className={classes.intro}>Hi, I am</h1>
-    <h2 className={classes.name}>Jelo Rivera.</h2>
-    <div className={classes.subtitle}>I tinker with everything web.</div>
-    <div className={classes.body}>
-      {`I am a software engineer whose craft involves building beautiful, stable
+    <div className={classes.content}>
+      <h1 className={classes.intro}>Hi, I am</h1>
+      <h2 className={classes.name}>Jelo Rivera.</h2>
+      <div className={classes.subtitle}>I tinker with everything web.</div>
+      <div className={classes.body}>
+        {`I am a software engineer whose craft involves building beautiful, stable
       and utilitarian digital platforms. Currently, I'm working on creating
       accessible, connection-centered digital products at `}
-      <span className={cs(classes.company, "clickable")}>Scentre Group</span>.
-    </div>
+        <span className={cs(classes.company, "clickable")}>Scentre Group</span>.
+      </div>
 
-    <Button className={classes.button}>
-      <a href={`mailto:${EMAIL}`}>Touch Base</a>
-    </Button>
+      <Button className={classes.button}>
+        <a href={`mailto:${EMAIL}`}>Touch Base</a>
+      </Button>
+    </div>
   </div>
 );
 
