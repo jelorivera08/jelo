8e9aec19e097514b79107722af04de1e89b4a8bd
jelo <jelorivera08@gmail.com>
2021-08-23 16:53:20 +0800

    added footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,18 @@ import Loader from "components/loader";
 import AppBar from "components/appbar";
 import { useRouter } from "next/router";
 import SideBars from "components/sidebars";
-import Intro from "components/intro";
+import Intro from "sections/intro";
 import { stripSpecialCharacters } from "lib/util";
 import { useEffect, useState } from "react";
-import Head from "next/head";
+import cs from "classnames";
 import classes from "styles/home.module.scss";
 import About from "sections/about";
 import Work from "sections/work";
 import Career from "sections/career";
 import Contact from "sections/contact";
+import Link from "next/link";
+import { GITHUB } from "lib/constant";
+import { FaGithub, FaGithubAlt } from "react-icons/fa";
 
 const Home: NextPage = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -27,15 +30,35 @@ const Home: NextPage = () => {
   }
 
   return (
-    <main className={classes.home}>
-      <AppBar />
-      <Intro />
-      <About asPath={asPath} />
-      <Career asPath={asPath} />
-      <Work asPath={asPath} />
-      <Contact />
-      <SideBars />
-    </main>
+    <>
+      <main className={classes.home}>
+        <AppBar />
+        <Intro />
+        <About asPath={asPath} />
+        <Career asPath={asPath} />
+        <Work asPath={asPath} />
+        <Contact asPath={asPath} />
+        <SideBars />
+      </main>
+      <footer className={classes.footer}>
+        <FaGithub
+          fill="#ccadbd"
+          fontSize="20px"
+          onClick={() => window.open(GITHUB, "_blank")}
+        />
+
+        <Link href={GITHUB}>
+          <a className={cs("clickable", classes.text)} target="_blank">
+            Designed & Built by Jelo Rivera
+          </a>
+        </Link>
+        <FaGithub
+          fill="#ccadbd"
+          fontSize="20px"
+          onClick={() => window.open(GITHUB, "_blank")}
+        />
+      </footer>
+    </>
   );
 };
 
