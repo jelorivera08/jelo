11e6af841c9fcf305177d970b40207423fc3285e
jelo <jelorivera08@gmail.com>
2021-09-06 12:02:08 +0800

    improve seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "lib/context/theme-provider";
 import SEO from "components/seo";
-import { DefaultSeo } from "next-seo";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
