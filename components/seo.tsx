d821b3ed72888dc644a34ffa6222343b2c9f686d
jelo <jelorivera08@gmail.com>
2021-09-12 16:10:38 +0800

    fix seo

--- components/seo.tsx
+++ components/seo.tsx
@@ -2,28 +2,26 @@ import { DefaultSeo } from "next-seo";
 
 export default function SEO() {
   return (
-    <>
-      <DefaultSeo
-        openGraph={{
-          profile: {
-            firstName: "Jelo",
-            lastName: "Rivera",
-            gender: "male",
-          },
-          type: "website",
-          locale: "en_US",
-          url: "https://jelo.studio",
-          site_name: "Jelo Rivera | Web Development",
-          title: "Jelo Rivera",
-          description:
-            "Jelo Rivera is a full stack web development engineer whose tech stack includes React, Javascript, Typescript, NodeJS, GraphQL and everything in between.",
-        }}
-        twitter={{
-          handle: "@jelonotgelo",
-          site: "https://twitter.com/jelonotgelo",
-          cardType: "summary_large_image",
-        }}
-      />
-    </>
+    <DefaultSeo
+      openGraph={{
+        profile: {
+          firstName: "Jelo",
+          lastName: "Rivera",
+          gender: "male",
+        },
+        type: "website",
+        locale: "en_US",
+        url: "https://jelo.studio",
+        site_name: "Jelo Rivera ",
+        title: "Jelo Rivera",
+        description:
+          "Jelo Rivera is a full stack web development engineer whose tech stack includes React, Javascript, Typescript, NodeJS, GraphQL and everything in between.",
+      }}
+      twitter={{
+        handle: "@jelonotgelo",
+        site: "https://twitter.com/jelonotgelo",
+        cardType: "summary_large_image",
+      }}
+    />
   );
 }
